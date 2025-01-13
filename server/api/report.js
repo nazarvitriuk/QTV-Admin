e1bc5280b4b8436ecba3138d492e9d8200a65b4c
nazar <nazar.vitriuk@4goo.com>
2025-01-13 11:23:23 +0200

    headers

---
+++ server/api/report.js
@@ -0,0 +1,8 @@
+export default defineEventHandler(async (event) => {
+  setHeader(event, 'Access-Control-Allow-Origin', '*');
+  setHeader(event, 'Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  setHeader(event, 'Access-Control-Allow-Headers', 'Content-Type');
+
+  const reqBody = await readBody(event);
+  console.log(reqBody);
+});
