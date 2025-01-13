e2a3f18c1aef699b8ef5e3fc9f248e5a4d38b2c9
nazar <nazar.vitriuk@4goo.com>
2025-01-13 11:33:39 +0200

    fix

--- server/api/report.js
+++ server/api/report.js
@@ -3,7 +3,12 @@ export default defineEventHandler(async (event) => {
   setHeader(event, 'Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   setHeader(event, 'Access-Control-Allow-Headers', 'Content-Type');
 
-  const reqBody = await readBody(event);
+  if (event._method === 'POST') {
+    const reqBody = await readBody(event);
 
-  return reqBody;
+    return reqBody;
+  }
+
+  return 'REPORT ROUTE'
+
 });
